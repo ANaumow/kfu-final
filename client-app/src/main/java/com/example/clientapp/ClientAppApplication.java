7bcb7502c50357dd4f047a873c02348b98d0ac06
Andrew Naumov <andrewnaumow@gmail.com>
2022-06-09 19:39:45 +0300

    changes

--- client-app/src/main/java/com/example/clientapp/ClientAppApplication.java
+++ client-app/src/main/java/com/example/clientapp/ClientAppApplication.java
@@ -2,10 +2,8 @@ package com.example.clientapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.itis.lib.EnableModules;
 
 @SpringBootApplication
-@EnableModules
 public class ClientAppApplication {
 
     public static void main(String[] args) {
